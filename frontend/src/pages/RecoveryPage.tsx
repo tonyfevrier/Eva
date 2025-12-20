c5ef6e30e72f5c01ceae0be2a6a2d95aaa7cd66f
FEVRIER <tony.fevrier62@gmail.com>
2025-12-20 13:10:31 -0500

    factor(react): factor Recovery Page

--- frontend/src/pages/RecoveryPage.tsx
+++ frontend/src/pages/RecoveryPage.tsx
@@ -1,21 +1,34 @@
-import React, { useState } from "react";
+import React, { useState, type Dispatch, type SetStateAction } from "react";
 import { Input } from "../components/Input";
 import { Button } from "../components/Button";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, type NavigateFunction } from "react-router-dom";
 
 export function RecoveryPage(){
     const [mailValue, setMailValue] = useState<string>("");
     const [fetchError, setFetchError] = useState<Error|null>(null);
     const navigate = useNavigate();
+
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         setMailValue(e.target.value);
     }
-    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         if (mailValue === ""){
             setFetchError(new Error("Vous devez remplir le champ"));
+            return;
         }
-        const response = await fetch("http://localhost:9000/auth/resetMail", {
+        sendPostRequest(mailValue, setFetchError, navigate);
+    }
+
+    return  <form action="" onSubmit={handleSubmit}>
+                <Input title="Veuillez entrer votre email" placeholder="email" name="email" value={mailValue} onChange={handleChange} type="text" disabled={false}/>
+                <Button>Envoyer un mail à l'adresse indiquée</Button>
+                {fetchError?.message && <p>{fetchError?.message}</p> }
+            </form>
+}
+
+async function sendPostRequest(mailValue:string, setFetchError:Dispatch<SetStateAction<Error|null>>, navigate: NavigateFunction){
+    const response = await fetch("http://localhost:9000/auth/resetMail", {
             method: "POST",
             headers:{
                 'Content-Type': 'application/json',
@@ -26,16 +39,11 @@ export function RecoveryPage(){
                 setFetchError(new Error(error.getMessage()))
                 throw error;
         });
-
-        if (response.ok){
-            navigate("/seeMail");    
-        } else {
-            setFetchError(new Error(`Erreur ${response.status}: ${response.statusText}`));
-        }
+    
+    if (response.ok){
+        navigate("/seeMail");    
+    } else {
+        setFetchError(new Error(`Erreur ${response.status}: ${response.statusText}`));
     }
-    return  <form action="" onSubmit={handleSubmit}>
-                <Input title="Veuillez entrer votre email" placeholder="email" name="email" value={mailValue} onChange={handleChange} type="text" disabled={false}/>
-                <Button>Envoyer un mail à l'adresse indiquée</Button>
-                {fetchError?.message && <p>{fetchError?.message}</p> }
-            </form>
+    return response
 }
