640c6e7fd2107e3f695414bae3bec47b04e166f8
FEVRIER <tony.fevrier62@gmail.com>
2026-01-31 19:23:40 -0500

    feat,style(react): create a FilteredSelector to have a select with a search bar and add it to FirstStep

--- frontend/src/pages/newExperimentationSubPages/FirstStep.tsx
+++ frontend/src/pages/newExperimentationSubPages/FirstStep.tsx
@@ -1,11 +1,14 @@
-import type { Dispatch, SetStateAction } from "react"
+import { useState, type Dispatch, type SetStateAction } from "react"
 import { Cloud } from "../../components/Cloud"
 import { Input } from "../../components/Input"
 import { Textarea } from "../../components/Textarea"
 import type { ExperimentationData } from "../NewExperimentationPage"
 import { useFetch } from "../../hooks/useFetch"
 import { Spinner } from "../../components/Spinner"
-import { Datalist } from "../../components/Datalist"
+import { Goto } from "../../components/Goto"
+import { FilteredSelector } from "../../components/FilteredSelector"
+import { Button } from "../../components/Button"
+import styles from "./FirstStep.module.css"
 
 type FirstStepState = {
     state: ExperimentationData;
@@ -15,7 +18,13 @@ type FirstStepState = {
 
 export function FirstStep({state, setState, handleClickOnCloud}:FirstStepState){
     const {loading, data, error} = useFetch<{institutions: Array<Record<string, any>>}>("http://localhost:9000/institution/getAll");
-    
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const handleChooseAffiliation = (e: React.MouseEvent<HTMLButtonElement>) => {
+        setState({...state, affiliation:{id:e.currentTarget.id, name:e.currentTarget.innerText}});
+        setIsModalOpen(false);
+    }
+
     if (loading){
         return <Spinner/>
     }
@@ -26,18 +35,20 @@ export function FirstStep({state, setState, handleClickOnCloud}:FirstStepState){
 
     if (data){
 
-        return <div>
-                <Datalist title="Etablissement de l'expérimentation" value={state.affiliation.name} onChange={e => setState({...state, affiliation:{id:e.target.id, name:e.target.value}})}>
-                    {data["institutions"].map(institution => (
-                        <option id={institution.id} key={institution.id} value={institution.name} />
-                    ))}
-                </Datalist>
-                <Cloud title="Choisissez éventuellement des mots clés" options={state.keywords} onClick={handleClickOnCloud}/>
-                <Input title="Autres mots clés personnalisés" value={state.personalKeywords} onChange={e => {setState({...state, personalKeywords: e.target.value})}}/>
-                <Textarea title="Ancienne pédagogie" variant="withErrorMsg" value={state.oldPedagogy} onChange={e => {setState({...state, oldPedagogy: e.target.value})}}/>
-                <Textarea title="Nouvelle pédagogie" variant="withErrorMsg" value={state.newPedagogy} onChange={e => {setState({...state, newPedagogy: e.target.value})}}/>
-                <Textarea title="Quelle difficulté d'apprentissage cette nouvelle pédagogie est-il supposé résoudre?" variant="withErrorMsg" value={state.learningDifficulty} onChange={e => {setState({...state, learningDifficulty: e.target.value})}}/>
-                <Textarea title="Cette difficulté étant probablement multimodale, quelle cause de cette difficulté est particulièrement visée par votre nouvelle pédagogie?" variant="withErrorMsg" value={state.learningDifficultyOrigin} onChange={e => {setState({...state, learningDifficultyOrigin: e.target.value})}}/>
-            </div>          
+        return  <div>
+                    <div className={styles.container}>
+                        <p>Choisissez votre affiliation</p>
+                        {state.affiliation.name !== "" && <p className={styles.institution}>{state.affiliation.name}</p>}
+                        <Button onClick={() => setIsModalOpen(true)}> {state.affiliation.name !== ""?"Modifiez votre choix":"Cliquez pour choisir"}</Button>
+                    </div>  
+                    {isModalOpen && <FilteredSelector title="Choisissez votre affiliation" items={data["institutions"]} onClick={handleChooseAffiliation} setIsModalOpen={setIsModalOpen}/>}
+                    <Goto href="/application/institution" label="Si vous n'avez pas trouvé votre établissement" buttonLabel="Créez-le ici"/>
+                    <Cloud title="Choisissez éventuellement des mots clés" options={state.keywords} onClick={handleClickOnCloud}/>
+                    <Input title="Autres mots clés personnalisés" value={state.personalKeywords} onChange={e => {setState({...state, personalKeywords: e.target.value})}}/>
+                    <Textarea title="Ancienne pédagogie" variant="withErrorMsg" value={state.oldPedagogy} onChange={e => {setState({...state, oldPedagogy: e.target.value})}}/>
+                    <Textarea title="Nouvelle pédagogie" variant="withErrorMsg" value={state.newPedagogy} onChange={e => {setState({...state, newPedagogy: e.target.value})}}/>
+                    <Textarea title="Quelle difficulté d'apprentissage cette nouvelle pédagogie est-il supposé résoudre?" variant="withErrorMsg" value={state.learningDifficulty} onChange={e => {setState({...state, learningDifficulty: e.target.value})}}/>
+                    <Textarea title="Cette difficulté étant probablement multimodale, quelle cause de cette difficulté est particulièrement visée par votre nouvelle pédagogie?" variant="withErrorMsg" value={state.learningDifficultyOrigin} onChange={e => {setState({...state, learningDifficultyOrigin: e.target.value})}}/>
+                </div>          
     }
 }
