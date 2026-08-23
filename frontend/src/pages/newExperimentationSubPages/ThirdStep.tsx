98eb9ca16fdcdfc5c547f99c14bb21722cdf8922
FEVRIER <tony.fevrier62@gmail.com>
2026-08-23 22:08:58 +0200

    debug(react): modify ThirdStep input fields

--- frontend/src/pages/newExperimentationSubPages/ThirdStep.tsx
+++ frontend/src/pages/newExperimentationSubPages/ThirdStep.tsx
@@ -46,17 +46,15 @@ export function ThirdStep({state, setState}:StepState){
                 {isModalOpen && <ModalFilteredSelector title="Choisissez votre affiliation" items={data["institutions"]} onClick={handleChooseAffiliation} setIsModalOpen={setIsModalOpen}/>}
                 <Goto variant="export" href="/application/institution" label="Si vous n'avez pas trouvé votre établissement" buttonLabel="Créez un établissement"/>
                 <LabeledFilteredInput title="Discipline enseignée" listTitle="De préférence, choisissez parmi" items={preRegisteredData["studyField"]} variant="withErrorMsg" value={state.studyField} onChange={e => {setState({...state, studyField: e.target.value})}}/>
-                {/* <Input title="Discipline enseignée" variant="withErrorMsg" value={state.studyField} onChange={e => {setState({...state, studyField: e.target.value})}}/> */}
-                <Input title="Intitulé ou thème de l'enseignement" variant="withErrorMsg" value={state.teachingTitle} onChange={e => {setState({...state, teachingTitle: e.target.value})}}/>
-                <Textarea title="Connaissances et/ou compétences à acquérir par les apprenants" variant="withErrorMsg" value={state.knowledges} onChange={e => {setState({...state, knowledges: e.target.value})}}/>
-                <Textarea title="Prérequis" variant="withErrorMsg" value={state.prerequisite} onChange={e => {setState({...state, prerequisite: e.target.value})}}/>
+                <Input title="Intitulé ou thème de l'enseignement (des enseignements pour la variante enseignement)" variant="withErrorMsg" value={state.teachingTitle} onChange={e => {setState({...state, teachingTitle: e.target.value})}}/>
+                <Textarea title="Connaissances et/ou compétences à acquérir durant l'enseignement (des enseignements pour la variante enseignement)" variant="withErrorMsg" value={state.knowledges} onChange={e => {setState({...state, knowledges: e.target.value})}}/>
+                <Textarea title="Prérequis de l'enseignement (des enseignements pour la variante enseignement)" variant="withErrorMsg" value={state.prerequisite} onChange={e => {setState({...state, prerequisite: e.target.value})}}/>
                 <Textarea title="Particularités de l'organisation du cours ou de la salle" variant="withErrorMsg" value={state.organisationParticularities} onChange={e => {setState({...state, organisationParticularities: e.target.value})}}/>
                 <Textarea title="Nombre, durée, horaire et fréquence des cours" variant="withErrorMsg" value={state.classesFrequencies} onChange={e => {setState({...state, classesFrequencies: e.target.value})}}/>
                 <Textarea title="Date(s) de l'enseignement" variant="withErrorMsg" value={state.classesDates} onChange={e => {setState({...state, classesDates: e.target.value})}}/>
                 <LabeledFilteredInput title="Année d'étude ou profession des apprenants" listTitle="De préférence, choisissez parmi" items={preRegisteredData["yearOfStudy"]} variant="withErrorMsg" value={state.yearOfStudy} onChange={e => {setState({...state, yearOfStudy: e.target.value})}}/>
-                {/* <Input title="Année d'étude ou profession des apprenants" variant="withErrorMsg" value={state.yearOfStudy} onChange={e => {setState({...state, yearOfStudy: e.target.value})}}/> */}
                 <Textarea title="Particularités des apprenants" variant="withErrorMsg" value={state.studentsSpecificities} onChange={e => {setState({...state,studentsSpecificities: e.target.value})}}/>
-                <Input title="Nombre d'apprenants" variant="withErrorMsg" value={state.studentsNumber} onChange={e => {setState({...state, studentsNumber: e.target.value})}}/>
+                <Input title="Nombre d'apprenants (des deux groupes pour les variantes groupe et année)" variant="withErrorMsg" value={state.studentsNumber} onChange={e => {setState({...state, studentsNumber: e.target.value})}}/>
             </div> 
         }
 }
